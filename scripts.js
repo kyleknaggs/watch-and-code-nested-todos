86bfcdef66f6812f818e3bdb7f7f1924b36bd88b
Kyle Knaggs <kyle@adfoam.com>
2019-09-01 21:57:09 -0400

    Attach code to window.nestedTodos.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,17 @@
-var todoList = {
-  todos: [],
-  addTodo: function(text){
-    todoList.todos.push(text);
-  }
-};
+(function(){
+
+  var todoList = {
+    todos: [],
+    addTodo: function (text) {
+      todoList.todos.push(text);
+    }
+  };
+
+  // Attach application code to a single key of the window object:
+  var nestedTodos = {
+    todoList
+  };
+
+  window.nestedTodos = nestedTodos;
+
+})();
