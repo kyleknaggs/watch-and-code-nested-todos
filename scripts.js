c394ad7196e39c612ac9a5d31af86090b12300e0
Kyle Knaggs <kyle@adfoam.com>
2019-09-05 08:45:22 -0400

    Write code to get 2 failing tests to pass.

--- scripts.js
+++ scripts.js
@@ -2,22 +2,44 @@
 
   var todoList = {
     todos: [],
-    add: function (text, id) {
-      var newTodo = {
-        text: text,
-        todos: []
+    add: function (text, indicies) {
+
+      var computedIndicies = [];
+
+      if(arguments.length > 1){
+        if(typeof(indicies) === "number"){
+          computedIndicies = [indicies]
+        }else{
+          computedIndicies = indicies;
+        }
       }
 
-      // Get list of todos:
-      var todos = todoList.todos;
+      var toModify;
 
-      // If an id is provided, nest the todo:
-      if(arguments.length === 1){
-        todos.push(newTodo);
-      }else{
-        todos[id].todos.push(newTodo);
+      // Manually set this when calling recursively.
+      // Guarantees all recursive calls will have a todos key.
+      if (this.todos) {
+        toModify = this.todos;
+      } else {
+        toModify = todoList.todos;
       }
 
+      if (computedIndicies.length === 0) {
+        // Base case:
+        var newTodo = {
+          text: text,
+          todos: []
+        };
+
+        toModify.push(newTodo);
+      } else {
+        // Recursive case:
+        var currentIndex = computedIndicies[0];
+        var currentTodo = toModify[currentIndex];
+        var remainingIndicies = computedIndicies.slice(1);
+
+        todoList.add.call(currentTodo, text, remainingIndicies);
+      }
     }
   };
 
