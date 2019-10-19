92661c5a2f9d2d2954d08b98819ce408dfd8395e
Kyle Knaggs <kyle@adfoam.com>
2019-10-19 11:22:01 -0400

    Make eq() an alias for assertStrictEquals().

--- _tests/simpletest.js
+++ _tests/simpletest.js
@@ -116,6 +116,6 @@ var TinyTest = {
 var fail = TinyTest.fail.bind(TinyTest),
   assert = TinyTest.assert.bind(TinyTest),
   assertEquals = TinyTest.assertEquals.bind(TinyTest),
-  eq = TinyTest.assertEquals.bind(TinyTest), // alias for assertEquals
   assertStrictEquals = TinyTest.assertStrictEquals.bind(TinyTest),
+  eq = TinyTest.assertStrictEquals.bind(TinyTest), // alias for assertStrictEquals
   tests = TinyTest.run.bind(TinyTest);
